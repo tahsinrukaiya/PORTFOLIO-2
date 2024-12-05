a67089690b431887cf480736637ce6b48b0da84c
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-12-05 21:53:38 +0100

    added icons

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -17,6 +17,8 @@ const Banner = () => {
                                 I want to acheive more beauty, creativity,
                                 and good ideas into the world through web design.
                             </p>
+                            <div className="text-start"><i className="icon fa-brands fa-github me-3"></i>
+                                <i className="icon fa-brands fa-linkedin"></i></div>
                         </div>
                         <div className="col">
                             <div className="banner-img-container"><img src={myImage} className="banner-img rounded-circle"></img></div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,13 @@ export default function Contact() {
     return (
         <>
             <div className="contact-container mb-5">
-                <h5 className="text-center">Conatact Info</h5>
+                <h2 className="text-center">Conatact Info</h2>
+                <ul className="contact-info text-center list-unstyled">
+                    <li>Mail Address : tahsinshorna@gmail.com</li>
+                    <li> Mobile : 0047-9941259</li>
+                    <li>LinkedIn : <a href="https://www.linkedin.com/in/rukaiya-tahsin">LinkedIn Profile
+                    </a></li>
+                </ul>
             </div>
         </>
     )
