11b459dc8cb7513d06079e326b0def4e4bdde731
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-12-05 17:27:05 +0100

    small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <div className="wave-container">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="wave-svg">
                     <path
-                        fill="#0B192C"
+                        fill="#100720"
                         d="M0,0 C480,50 960,50 1440,0 L1440,120 L0,120 Z"
                     ></path>
                 </svg>
