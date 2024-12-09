d9b08e7df6017a40b4b819a5ff21f1a6d361ad13
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-12-09 19:41:07 +0100

    added responsiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,10 +7,10 @@ const Banner = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col">
-                            <h3 className="logo mt-3 text-start">RT</h3>
+                            <h3 className="logo mt-3 text-start">Rukaiya Tahsin</h3>
                             <h2 className="heading-one text-start mt-5">Welcome</h2>
-                            <span className="slogan"> Crafting User-Centered Designs with Clean Code</span><br />
-                            <p className="intro-name">Rukaiya Tahsin || Frontend Developer</p>
+                            <span className="slogan text-start"> Crafting User-Centered Designs with Clean Code</span><br />
+                            <p className="intro-name text-start">Frontend Developer</p>
                             <div className="text-start">
                                 <a href="https://github.com/tahsinrukaiya"><i className="icon fa-brands fa-github me-3 mt-3"></i></a>
                                 <a href="https://www.linkedin.com/in/rukaiya-tahsin/"><i className="icon fa-brands fa-linkedin"></i></a></div>
